706dc73d47c9afaeb8ca61569f3c429fbb1cc30e
sowmyamamillapalli <147277467+sowmyamamillapalli@users.noreply.github.com>
2024-05-28 00:37:34 +0200

    Update inspectionRController.js

--- controllers/inspectionRController.js
+++ controllers/inspectionRController.js
@@ -19,7 +19,7 @@ const prepareImageForm = (file) => {
 const detectImage = async (imageForm) => {
   try {
     const response = await axios.post(
-      "http://localhost:5000/v1/inspection/best",
+      "137.184.185.56:8000/v1/inspection/best",
       imageForm,
       {
         headers: { ...imageForm.getHeaders() },
